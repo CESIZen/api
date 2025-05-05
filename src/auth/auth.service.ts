8219cf54361ddca78cdba9a544ef38032bcfc557
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-05-06 00:23:23 +0200

    feat: ajout d'info dans le token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,7 +31,7 @@ export class AuthService {
 
   async login(user: any) {
     console.log('login');
-    const payload = { email: user.email, sub: user.id };
+    const payload = { email: user.email, sub: user.id, roleId: user.roleId, name: user.name };
     return {
       access_token: this.jwtService.sign(payload),
     };

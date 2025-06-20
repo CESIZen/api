e63f9bb8e880242574cc5fe4ac651b0927a51277
Joseph MAUJEAN <joseph.maujean@viacesi.fr>
2025-06-20 23:11:01 +0200

    fix(ci-cd): npm audit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
